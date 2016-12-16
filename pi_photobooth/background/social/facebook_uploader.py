ea7e05d705a614853ff177d43e4794ba6f742acd
Christopher <cgamble@salesforce.com>
2016-12-15 16:52:24 -0800

    removed unused function for better coverage, ahyea

--- pi_photobooth/background/social/facebook_uploader.py
+++ pi_photobooth/background/social/facebook_uploader.py
@@ -9,31 +9,31 @@ FACEBOOK_PROFILE_ID = ""
 FACEBOOK_ACCESS_TOKEN = ""
 class FacebookUploader:
 
-    def login(self):
-        print("Connecting to Facebook")
-        oauth_args = dict(client_id     = FACEBOOK_APP_ID,
-                        client_secret = FACEBOOK_APP_SECRET,
-                        grant_type    = 'client_credentials')
-        oauth_curl_cmd = ['curl',
-                        'https://graph.facebook.com/oauth/access_token?' + urllib.urlencode(oauth_args)]
+    # def login(self):
+    #     print("Connecting to Facebook")
+    #     oauth_args = dict(client_id     = FACEBOOK_APP_ID,
+    #                     client_secret = FACEBOOK_APP_SECRET,
+    #                     grant_type    = 'client_credentials')
+    #     oauth_curl_cmd = ['curl',
+    #                     'https://graph.facebook.com/oauth/access_token?' + urllib.urlencode(oauth_args)]
 
-        oauth_response = subprocess.Popen(oauth_curl_cmd,
-                                        stdout = subprocess.PIPE,
-                                        stderr = subprocess.PIPE).communicate()[0]
-        try:
-            print("Logging in")
+    #     oauth_response = subprocess.Popen(oauth_curl_cmd,
+    #                                     stdout = subprocess.PIPE,
+    #                                     stderr = subprocess.PIPE).communicate()[0]
+    #     try:
+    #         print("Logging in")
 
-            oauth_access_token = urlparse.parse_qs(str(oauth_response))['access_token'][0]
-        except KeyError:
-            print('Unable to grab an access token!')
-            exit()
-        print ("Successfully Logged in")
-        return oauth_access_token
+    #         oauth_access_token = urlparse.parse_qs(str(oauth_response))['access_token'][0]
+    #     except KeyError:
+    #         print('Unable to grab an access token!')
+    #         exit()
+    #     print ("Successfully Logged in")
+    #     return oauth_access_token
 
     def __init__(self, graph=facebook.GraphAPI(FACEBOOK_ACCESS_TOKEN)):
         print("Initializing Uploader")
         self.events = None
-       #self.token = self.login()
+        #self.token = self.login()
         self.graph = graph
         print("Connected to Facebook")
 
